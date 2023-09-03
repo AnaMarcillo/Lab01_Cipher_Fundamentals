d605aa3c2b2b19434168e579ef2d543f28e0d0b7
AnaMarcillo <138828744+AnaMarcillo@users.noreply.github.com>
2023-09-03 12:55:46 -0500

    Update Decrypt.py

--- Decrypt.py
+++ Decrypt.py
@@ -1,17 +1,20 @@
-"""Bruteforce Attack"""
 def caesar_decrypt(ciphertext, shift):
     decrypted_text = ""
     for char in ciphertext:
         if char.isalpha():
-            shifted_char = chr(((ord(char) - ord('a') - shift) % 26) + ord('a'))
+            if char.islower():
+                shifted_char = chr(((ord(char) - ord('a') - shift) % 26) + ord('a'))
+            else:
+                shifted_char = chr(((ord(char) - ord('A') - shift) % 26) + ord('A'))
             decrypted_text += shifted_char
         else:
             decrypted_text += char
     return decrypted_text
 
-ciphertext = """o ymnx nx ymj ktwjxy uwnrjafq"""
+ciphertext = """WKH TXLFN EURZQ IRA MXPSV RYHU WKH ODCB GRJ"""
 
 # Try all possible shifts (0 to 25)
 for shift in range(26):
     decrypted_text = caesar_decrypt(ciphertext, shift)
-    print(f"Key {shift}, plaintext: {decrypted_text}")
+    print(f"Shift {shift}: {decrypted_text}")
+
